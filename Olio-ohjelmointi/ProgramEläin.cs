671b8910ac37740912f564bf1de0ba96fea5c105
Lonnale <77781939+Lonnale@users.noreply.github.com>
2022-03-07 15:11:33 +0200

    Add files via upload

---
+++ Olio-ohjelmointi/ProgramEläin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eläinvakuutukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsuranceManager im = new InsuranceManager();
+            im.AddInsurance("koira", "Rolle", false);
+            im.AddInsurance("koira", "Tessu", true);
+            im.AddInsurance("kissa", "Monni", true);
+            im.AddInsurance("koira", "Pluto", false);
+            im.AddInsurance("lintu", "Tipi", false);
+            im.PrintInsurances();
+            Console.WriteLine("---");
+            im.FindInsurances("koira", false);
+        }
+    }
+}
