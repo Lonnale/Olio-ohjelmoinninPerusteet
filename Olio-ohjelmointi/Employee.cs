9311ee56a5dea37aec1c1fb97091ddec9b2a5c36
Lonnale <77781939+Lonnale@users.noreply.github.com>
2022-03-07 15:06:40 +0200

    Add files via upload

---
+++ Olio-ohjelmointi/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_programing
+{
+    class Employee
+    {
+        public string name;
+        public string id;
+        public string position;
+        public double salary;
+      
+
+        
+
+        public Employee(string name, string id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+              
+        public void comparesalary(Employee employee)
+        {
+
+            Console.WriteLine("kutsuvan olion title: " + this.name + ", " +
+              "parametrina välitettävän olion title: "+employee.name);
+
+
+
+            if (salary > employee.salary)
+            {
+                Console.WriteLine(this.salary + " on kalliimpi kuin " + employee.salary);
+
+            }
+            else if (this.salary < employee.salary)
+            {
+                Console.WriteLine(employee.salary + " on kalliimpi kuin " + this.salary);
+            }
+            else if (this.salary == employee.salary)
+            {
+                Console.WriteLine("Hinnat yhtäsuuret");
+            } 
+
+
+        }
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijä: {this.name} , {this.id} , {this.position} , {this.salary}");         
+        }
+
+        public override string ToString()
+        {
+            return $"Työntekijä: {this.name} , {this.id} , {this.position} , {this.salary}";
+        }
+    }
+   
+
+
+}
---
+++ Olio-ohjelmointi/Programemployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_programing
+{
+    static void EmployeeExample()
+    {
+
+
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee("Aleksi", "12345", "Toimitusjohtaja", 1500000);
+        employees[1] = new Employee("Pertti", "15321", "kirjanpitäjä", 5500);
+        employees[2] = new Employee("Masi", "12356", "It-Arkkitehti", 3300);
+
+
+
+        foreach (Employee vol in employees)
+        {
+            Console.WriteLine(vol.ToString());
+
+        }
+        Console.WriteLine("");
+        employees[0].comparesalary(employees[1]);
+        for (int i = 0; i < employees.Length; i++)
+        {
+
+
+            if (i == employees.Length - 1)
+            {
+                employees[i].comparesalary(employees[0]);
+            }
+            else
+            {
+                employees[i].comparesalary(employees[i + 1]);
+            }
+        }
+
+    }
+}
